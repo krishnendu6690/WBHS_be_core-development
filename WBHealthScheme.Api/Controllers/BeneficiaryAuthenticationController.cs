884d24d4011211b15f4bbb01349706962eea5a1c
pauldebbrata <pauldebpaul@hotmail.com>
2026-04-22 12:31:39 +0530

    commit 22042026 Broto

--- WBHealthScheme.Api/Controllers/BeneficiaryAuthenticationController.cs
+++ WBHealthScheme.Api/Controllers/BeneficiaryAuthenticationController.cs
@@ -34,20 +34,12 @@ namespace WBHealthScheme.Api.Controllers
 
         [HttpGet("APPID/{enrollmentid}")]
         public async Task<IActionResult> GetwardByappid(string enrollmentid)
-    {
-    var result = await
-    _service.GetwardByappAsync(enrollmentid);
+        {
+        var result = await
+        _service.GetwardByappAsync(enrollmentid);
 
-    //return Ok(new ApiResponse<List<BeneficiaryAuthenticationResponse>>
-    //{
-    //    Success = true,
-    //    Message = "Beneficiary fetched successfully",
-    //    Status = "200",
-    //    Data = result,
-    //    Errors = null
-    //});
-    return Ok(ApiResponse<List<Beneiciary_ward_resp_broto>>
+        return Ok(ApiResponse<List<Beneiciary_ward_resp_broto>>
         .Ok(result, "Enrollment fetched successfully"));
-}
+        }
     }
 }
--- WBHealthScheme.Application/Services/BeneficiaryAuthenticationService.cs
+++ WBHealthScheme.Application/Services/BeneficiaryAuthenticationService.cs
@@ -27,18 +27,18 @@ namespace WBHealthScheme.Application.Services
             return result;
         }
          public async Task<List<Beneiciary_ward_resp_broto>>
- GetwardByappAsync(string app_ID)
- {
-     if (string.IsNullOrWhiteSpace(app_ID))
-         throw new BusinessRuleException("Enrollment ID is required");
-     if (app_ID.Length != 10 || !app_ID.All(char.IsDigit))
-         throw new BusinessRuleException("Invalid Enrollment ID");
-     var result = await
-         _repository.GetwardByappAsync(app_ID);
-     if (result == null || !result.Any())
-         throw new NotFoundException("Enrollment ID not found");
-     return result;
- }
+        GetwardByappAsync(string app_ID)
+        {
+            if (string.IsNullOrWhiteSpace(app_ID))
+            throw new BusinessRuleException("Enrollment ID is required");
+            if (app_ID.Length != 10 || !app_ID.All(char.IsDigit))
+            throw new BusinessRuleException("Invalid Enrollment ID");
+            var result = await
+            _repository.GetwardByappAsync(app_ID);
+            if (result == null || !result.Any())
+            throw new NotFoundException("Enrollment ID not found");
+            return result;
+        }
         
         
     }
